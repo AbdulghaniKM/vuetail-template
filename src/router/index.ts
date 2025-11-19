019fd1263731449bd001e01962061fc098ce435c
AbdulghaniKM <abdulghanim70@gmail.com>
2025-11-19 21:52:29 +0300

    feat: :sparkles: add smooth scroll behavior to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/home.vue';
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: '/', name: 'home', component: Home },
-  ],
+  routes: [{ path: '/', name: 'home', component: Home }],
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0, behavior: 'smooth' };
+  },
 });
+
 export default router;
